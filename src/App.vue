09a861010160e7670609cf51342f5c43cfc3e13e
wkellen <410622677@qq.com>
2019-09-25 14:59:27 +0800

    把项目提交到git，并写好公共样式

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="colorRed">
+
+      <div class="box"></div>
+      <router-link to="/login">登录页</router-link>
+      <router-view></router-view>
+
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+    /* 公共样式 */
+    *{
+        margin: 0;
+        padding: 0;
+    }
+
+    a{
+        color:inherit;
+    }
+    a:hover{
+        text-decoration: none;
+    }
+    ol,ul,li{
+        list-style: none;
+    }
+    i,em{
+        font-style: normal;
+    }
+</style>
